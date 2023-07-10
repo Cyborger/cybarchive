5f66b4f1a8366b1ebb0920bcbd9ff97d44cdbb09
Romain Seznec <contact@romainseznec.com>
2023-07-10 15:19:19 +0200

    feat: added gpg support

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ from utils import *
 
 
 @click.command()
-@click.option("-o", "--output_filename", default="output.tar.gz")
+@click.option("-e", "--encrypt")
+@click.option("-n", "--name")
 @click.argument("source_path", type=click.Path(exists=True))
-def main(output_filename, source_path):
+def main(name, encrypt, source_path):
     click.echo("Archiving " + click.format_filename(source_path))
-    archive_and_compress(output_filename, source_path)
+
+    tgz_archive_path = archive_and_compress(name, source_path)
+    click.echo("File archived to " + tgz_archive_path)
+
+    if encrypt is None:
+        return
+
+    gpg_archive_path = encrypt_file(tgz_archive_path, encrypt)
+    click.echo("File encrypted to " + gpg_archive_path)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,7 +1,28 @@
 import os.path
 import tarfile
+import tempfile
+import datetime
+import gnupg
 
 
-def archive_and_compress(output_filename, source_path):
-    with tarfile.open(output_filename, "w:gz") as tar:
+def archive_and_compress(name, source_path):
+    current_time = datetime.datetime.now().isoformat()
+    filename = f"cybarchive{'-' + name if name is not None else ''}-{current_time}.tar.gz"
+    save_path = os.path.join(tempfile.gettempdir(), filename)
+
+    with tarfile.open(save_path, "w:gz") as tar:
         tar.add(source_path, arcname=os.path.basename(source_path))
+
+    return save_path
+
+
+def encrypt_file(source_path, key_fingerprint):
+    gpg = gnupg.GPG()
+    save_path = source_path + ".gpg"
+
+    encrypted_data = gpg.encrypt_file(open(source_path, "rb"), key_fingerprint, output=save_path)
+
+    if not encrypted_data.ok:
+        raise Exception(encrypted_data.status)
+
+    return save_path
